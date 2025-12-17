ceb25aff2d5a8b5462429cf51acd9a09574e0c80
Charlie <pmbarrons@gmail.com>
2025-12-17 12:57:38 -0700

    makeing som changes

---
+++ sh-files/svg-changer.sh
@@ -0,0 +1,2 @@
+npm install vite-plugin-svgr --save-dev
+# or yarn add vite-plugin-svgr -D
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
+
+
+
+// this below is for the public that dose not work yet, and by yet i mean yet
+
 export default {
     base: "/brick-basher/"
 }
+
+
