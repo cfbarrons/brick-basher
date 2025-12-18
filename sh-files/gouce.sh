0792af48583ee1d10edbcab6644b4c43009dc4d0
Charlie <pmbarrons@gmail.com>
2025-12-18 12:22:55 -0700

    installing gource

---
+++ sh-files/gouce.sh
@@ -0,0 +1,16 @@
+This is how you get gource on chrome book
+
+1.LS
+
+2.cd my-project
+//Instead of my-project put brick basher or the file that you are 
+working
+
+3.ls -a
+
+4.git init
+git add .
+git commit -m "initial test"
+
+5.export LIBGL_ALWAYS_SOFTWARE=1
+gource -640x480
--- sh-files/svg-changer.sh
+++ sh-files/svg-changer.sh
@@ -1,2 +1,4 @@
 npm install vite-plugin-svgr --save-dev
 # or yarn add vite-plugin-svgr -D
+
+# this dose not work yet still working on it
